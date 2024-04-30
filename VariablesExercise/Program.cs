a7354cf9de4dcbce9c7032f814ad2436ab8d7518
Irfan <irfanullahkhattak744@gmail.com>
2024-04-29 22:40:36 -0400

    Done with the varibales exercise

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dogName = "Ralph";
+            int dogAge = 2;
+            char inital = 'R';
+            double eatingCost = 19.9;
+            bool is1YearOld = true;
+            decimal price = 200.00m;
+            Console.WriteLine($"My dog's name is {dogName}, He is {dogAge} years old");
+            Console.WriteLine($" {dogName}'s initail are, just {inital}");
+            Console.WriteLine($" {dogName}'s eating cost per week is {eatingCost}");
+            Console.WriteLine($" Is {dogName} over 1 ?: {is1YearOld}");
+            Console.WriteLine($"I bought {dogName} for ${price}, when he was a babay");
+
         }
     }
 }
